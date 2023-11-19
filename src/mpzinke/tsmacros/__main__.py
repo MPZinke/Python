c4c515c576ce679d80d3cc3017f99306ba58a268
mpzinke <mathew.zinke@gmail.com>
2023-11-19 12:23:53 -0600

    Converts string abs path to Path object

--- src/mpzinke/tsmacros/__main__.py
+++ src/mpzinke/tsmacros/__main__.py
@@ -9,6 +9,6 @@ from .RunMacros import run_macros
 
 
 assert(len(sys.argv) == 2), "Usage: tsmacros <mapping_json_path> [<source_directory_path>]"
-mappings_file_path = os.path.join(os.getcwd(), Path(sys.argv[1]))
+mappings_file_path = Path(os.path.join(os.getcwd(), Path(sys.argv[1])))
 
 run_macros(mappings_file_path)
